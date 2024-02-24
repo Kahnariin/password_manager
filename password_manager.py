7d9671323a2f22731c9aac92edca1ef2da017413
Kahnariin <m.ikbalozsoy@gmail.com>
2024-02-24 23:05:07 +0300

    initial commit

--- password_manager.py
+++ password_manager.py
@@ -1,12 +1,11 @@
 from cryptography.fernet import Fernet
 
-
-
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
+
 
 def load_key():
     file = open("key.key", "rb")
@@ -14,28 +13,31 @@ def load_key():
     file.close()
     return key
 
-master_pwd = input("What is the master password? ")
-key = load_key() + master_pwd.encode
+
+key = load_key()
 fer = Fernet(key)
 
 
 def view():
-    with open('password.txt', "r") as r:
-        for line in r.readlines():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "Password:", str(fer.encrypt(passw.encode())))
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
 
 def add():
     name = input('Account Name: ')
-    pwd = input('Password: ')
+    pwd = input("Password: ")
 
-    with open('password.txt', "a") as f: # open() fonksiyonu dosyaları açmak için kullanılır. 3 modu bulunur "w" modu dosyaları mevcutsa üzerine yazar değilse sıfırdan oluşturur. "r" modu sadece dosyayı okur ama yazamaz. "a" modu hem yazar hem de okur ama dosyanın üzerine yazmaz.
-        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones? (view, add) press q to quit. ").lower()
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode == "q":
         break
 
